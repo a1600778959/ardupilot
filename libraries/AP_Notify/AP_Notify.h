09a4e8828043404c8f8e3b37b79650916b24257a
Peter Barker <pbarker@barker.dropbear.id.au>
2023-02-27 15:06:38 +1100

    AP_Notify: add driver for LP5562 LED

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -82,6 +82,7 @@ public:
 #endif
 #if AP_NOTIFY_DSHOT_LED_ENABLED
         Notify_LED_DShot                    = (1 << 11),// Use dshot commands to set ESC LEDs
+<<<<<<< HEAD
 #endif
 #if AP_NOTIFY_PROFILED_SPI_ENABLED
         Notify_LED_ProfiLED_SPI             = (1 << 12), // ProfiLED (SPI)
@@ -99,6 +100,12 @@ public:
 #endif
 #if AP_NOTIFY_NEOPIXEL_ENABLED
         Notify_LED_NeoPixelRGB              = (1 << 18), // NeoPixel AdaFruit 4544 Worldsemi WS2811
+=======
+        Notify_LED_ProfiLED_SPI             = (1 << 12), // ProfiLED
+#if AP_NOTIFY_LP5562_ENABLED
+        Notify_LED_LP5562_I2C_External          = (1 << 13), // LP5562
+        Notify_LED_LP5562_I2C_Internal          = (1 << 14), // LP5562
+>>>>>>> fa9cb45fcb (AP_Notify: add driver for LP5562 LED)
 #endif
         Notify_LED_MAX
     };
