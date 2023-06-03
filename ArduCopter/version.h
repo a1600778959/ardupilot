cf5f27bfd401b1f679067c4f86bb1b9bf3e1fc3b
Randy Mackay <rmackay9@yahoo.com>
2023-06-03 11:21:26 +0900

    Copter: version to 4.4.0-beta2

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,10 +6,17 @@
 
 #include "ap_version.h"
 
+<<<<<<< HEAD
 #define THISFIRMWARE "ArduCopter V4.5.7"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_OFFICIAL
+=======
+#define THISFIRMWARE "ArduCopter V4.4.0-beta2"
+
+// the following line is parsed by the autotest scripts
+#define FIRMWARE_VERSION 4,4,0,FIRMWARE_VERSION_TYPE_BETA+2
+>>>>>>> d8a35e5dec (Copter: version to 4.4.0-beta2)
 
 #define FW_MAJOR 4
 #define FW_MINOR 5
