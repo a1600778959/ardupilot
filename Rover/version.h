106404f9b943a9bdef21ac45c567cd3443ec1213
Randy Mackay <rmackay9@yahoo.com>
2023-06-03 11:20:36 +0900

    Rover: version to 4.4.0-beta2

--- Rover/version.h
+++ Rover/version.h
@@ -6,10 +6,17 @@
 
 #include "ap_version.h"
 
+<<<<<<< HEAD
 #define THISFIRMWARE "ArduRover V4.5.7"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_OFFICIAL
+=======
+#define THISFIRMWARE "ArduRover V4.4.0-beta2"
+
+// the following line is parsed by the autotest scripts
+#define FIRMWARE_VERSION 4,4,0,FIRMWARE_VERSION_TYPE_BETA+2
+>>>>>>> a8f2a6d14b (Rover: version to 4.4.0-beta2)
 
 #define FW_MAJOR 4
 #define FW_MINOR 5
