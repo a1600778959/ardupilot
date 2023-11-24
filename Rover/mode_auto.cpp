91c4a122c239942845666371791198cc9a26db5b
Randy Mackay <rmackay9@yahoo.com>
2023-11-24 10:10:11 +0900

    Rover: auto navigates while stopped

--- Rover/mode_auto.cpp
+++ Rover/mode_auto.cpp
@@ -83,6 +83,7 @@ void ModeAuto::update()
         case SubMode::WP:
         {
             // boats loiter once the waypoint is reached
+<<<<<<< HEAD
             bool keep_navigating = true;
             if (rover.is_boat() && g2.wp_nav.reached_destination() && !g2.wp_nav.is_fast_waypoint()) {
                 keep_navigating = !start_loiter();
@@ -90,6 +91,15 @@ void ModeAuto::update()
 
             // update navigation controller
             if (keep_navigating) {
+=======
+            if (rover.is_boat() && g2.wp_nav.reached_destination() && !g2.wp_nav.is_fast_waypoint()) {
+                start_loiter();
+
+                // update distance to destination
+                _distance_to_destination = rover.current_loc.get_distance(g2.wp_nav.get_destination());
+            } else {
+                // update navigation controller
+>>>>>>> 3d342604f8 (Rover: auto navigates while stopped)
                 navigate_to_waypoint();
             }
             break;
